0a109a0353e77feb414fd0144f406a8987f333f6
veeps <vpeng12@gmail.com>
2018-11-30 14:10:10 -0800

    GMaps API working now. Cleaned data table and select for US only. Mapped locations to map. Write out table into CSV for future.

--- twitte.R
+++ twitte.R
@@ -4,9 +4,10 @@ library(digest)
 library(SnowballC)
 library(tm)
 library(syuzhet)
+library(maps)
+library(mapproj)
 
-
-
+setwd("git/VeepsLearnsTwitteR")
 ## Twitter Authentication
 
 if (!require("twitteR")) {
@@ -137,3 +138,37 @@ condition_b <- lapply(geocode_results, lapply, length)
 condition_b2<-sapply(condition_b, function(x) x["results"]=="1") #remove everything with status ANY
 geocode_results<-geocode_results[condition_b2]
 length(geocode_results)
+
+
+## clean misformatting
+source("https://raw.githubusercontent.com/LucasPuente/geocoding/master/cleaning_geocoded_results.R")
+results_b<-lapply(geocode_results, as.data.frame)
+
+
+results_c <-lapply(results_b,function(x) subset(x, select=c("results.formatted_address",
+                                                           "results.geometry.location")))
+
+results_d<-lapply(results_c,function(x) data.frame(Location=x[1,"results.formatted_address"],
+                                                   lat=x[1,"results.geometry.location"],
+                                                   lng=x[2,"results.geometry.location"]))
+
+results_e<-rbindlist(results_d)
+
+#add in original location column
+results_f<-results_e[,Original_Location:=names(results_d)]
+
+#write results into CSV for future
+write.csv(results_f, file="VeepsTwitterFollowers.csv")
+
+
+#filter for US locations only
+american_results<-subset(results_f,
+                         grepl(", USA", results_f$Location)==TRUE)
+
+
+#generate blank map
+albers_proj<-map("state", proj="albers", param=c(39, 45), 
+                 col="#999999", fill=FALSE, bg=NA, lwd=0.2, add=FALSE, resolution=1)
+
+#add points to map
+points(mapproject(american_results$lng, american_results$lat), col=NA, bg="#D3D3D3", pch=21, cex=1.0)
